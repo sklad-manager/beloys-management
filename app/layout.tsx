29bb68c8bdc1532a536ad398667ce9b816a6d21a
Belotex <belotex@example.com>
2025-12-05 00:02:32 +0200

    Apply Argon Dashboard theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const openSans = Open_Sans({
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-open-sans",
 });
 
 export const metadata: Metadata = {
@@ -36,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${openSans.variable} font-sans antialiased`}
       >
         {children}
       </body>
